a0b0fbb91e9825d8d6ca977038067f39531f639b
Xaspy <64539300+Xaspy@users.noreply.github.com>
2020-10-13 23:37:14 +0500

    begun response class code

---
+++ model/response.py
@@ -0,0 +1,11 @@
+from ws_exceptions.ws_exceptions import BadResponse
+from model.request import Request
+
+
+class Response:
+    def __init__(self, request: Request):
+        pass
+
+
+if __name__ == '__main__':
+    pass
--- ws_exceptions/ws_exceptions.py
+++ ws_exceptions/ws_exceptions.py
@@ -4,5 +4,10 @@ class BadRequest(Exception):
         self.message = message
 
 
+class BadResponse(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
 if __name__ == '__main__':
     pass
