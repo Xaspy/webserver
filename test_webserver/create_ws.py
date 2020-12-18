6ff723530e94a498d05681c20c3f80cc487563e1
Xaspy <64539300+Xaspy@users.noreply.github.com>
2020-12-18 21:32:46 +0500

    test fix

--- test_webserver/create_ws.py
+++ test_webserver/create_ws.py
@@ -4,17 +4,17 @@ app = Xio(__name__)
 
 
 @app.route('/test-page')
-def test_page():
+async def test_page():
     return 'all-right'
 
 
 @app.route('/test-page-get')
-def test_page():
+async def test_page():
     return 'get ok'
 
 
 @app.route('/test-page-post', ['POST'])
-def test_page(data):
+async def test_page(data):
     return 'post ok ' + data
 
 
